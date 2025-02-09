ded7fe08f8047995d5b2825bf42f72693f6f0a3b
Jasper Green <elvi.green11@gmail.com>
2025-02-09 15:05:55 +0800

    Changed server port to 8080

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const handlebars = require('handlebars'); 
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 8080;
 
 // Give the pages access to files
 app.use(express.static('public'));
